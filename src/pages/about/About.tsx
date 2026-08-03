3dc92f60c462c1d9f57c45c3266651f3e40980e6
utkarshgogna1 <gognautkarsh@gmail.com>
2026-08-02 21:08:15 -0400

    Fix build: remove unused Blog import

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,7 +3,6 @@ import { FC } from "react";
 import { Box, Flex, Heading, Text, Image, Button, IconButton } from "@chakra-ui/react";
 
 import { configs, Content, MarkdownFile, useContent } from "shared/content/Content";
-import { Blog } from "pages/about/blog/Blog";
 import { Education } from "pages/about/education/Education";
 import { Experience } from "pages/about/experience/Experience";
 import { Skills } from "pages/about/skills/Skills";
